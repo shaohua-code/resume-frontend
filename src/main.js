b010370bf7896fabe43cc09487ef121a40a74e75
刘少华 <1377250790@qq.com>
2026-06-27 14:30:38 +0800

    feat: 验证是否重新部署 0000

--- src/main.js
+++ src/main.js
@@ -15,6 +15,7 @@ import './styles/global.css'
 dayjs.locale('zh-cn')
 
 const app = createApp(App)
+console.log('Vite API URL:', import.meta.env.VITE_API_URL);
 
 app.use(createPinia()) // Pinia 状态管理
 app.use(router) // Vue Router 路由
